ab872a8362b85890d486d6195855747cdb33360d
MilhouseVH <milhouseVH.github@nmacleod.com>
2017-09-01 00:30:14 +0100

    heimdal: auto-refactor for Python3

--- packages/devel/heimdal/package.mk
+++ packages/devel/heimdal/package.mk
@@ -24,7 +24,7 @@ PKG_LICENSE="BSD-3c"
 PKG_SITE="http://www.h5l.org/"
 PKG_URL="https://github.com/heimdal/heimdal/archive/$PKG_NAME-$PKG_VERSION.tar.gz"
 PKG_SOURCE_DIR="$PKG_NAME-$PKG_NAME-$PKG_VERSION"
-PKG_DEPENDS_HOST="toolchain e2fsprogs:host"
+PKG_DEPENDS_HOST="toolchain e2fsprogs:host Python3:host"
 PKG_SECTION="devel"
 PKG_SHORTDESC="heimdal: Kerberos 5, PKIX, CMS, GSS-API, SPNEGO, NTLM, Digest-MD5 and, SASL implementation."
 PKG_LONGDESC="Heimdal is an implementation of Kerberos 5 (and some more stuff) largely written in Sweden (which was important when we started writing it, less so now). It is freely available under a three clause BSD style license. "
@@ -47,6 +47,12 @@ PKG_CONFIGURE_OPTS_HOST="--enable-static --disable-shared \
                          --without-x \
                          --with-db-type-preference= \
                          --disable-heimdal-documentation"
+
+post_patch() {
+  # Automatically refactor python2 code to python3
+  $TOOLCHAIN/bin/2to3-$(get_pkg_variable Python3 PKG_PYTHON_VERSION) --processes=$(nproc) --write --nobackups --no-diffs $PKG_BUILD
+  $TOOLCHAIN/bin/reindent.py --recurse --nobackup $PKG_BUILD
+}
 
 makeinstall_host() {
   mkdir -p $TOOLCHAIN/bin
