e28b5557ec80f33f34b7b6d6a7a8538e181cf4e3
MilhouseVH <milhouseVH.github@nmacleod.com>
2017-09-01 02:12:32 +0100

    Python: update from v2 to v3

--- packages/lang/Python/package.mk
+++ packages/lang/Python/package.mk
@@ -17,53 +17,90 @@
 ################################################################################
 
 PKG_NAME="Python"
-PKG_VERSION="2.7.13"
-PKG_SHA256="35d543986882f78261f97787fd3e06274bfa6df29fac9b4a94f73930ff98f731"
+PKG_VERSION="3.6.2"
+PKG_SHA256="9229773be41ed144370f47f0f626a1579931f5a390f1e8e3853174d52edd64a9"
 PKG_ARCH="any"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.python.org/"
 PKG_URL="http://www.python.org/ftp/python/$PKG_VERSION/$PKG_NAME-$PKG_VERSION.tar.xz"
-PKG_DEPENDS_HOST="zlib:host bzip2:host sqlite:host"
-PKG_DEPENDS_TARGET="toolchain sqlite expat zlib bzip2 openssl libffi Python:host"
+PKG_DEPENDS_HOST="zlib:host bzip2:host"
+PKG_DEPENDS_TARGET="toolchain sqlite expat zlib bzip2 openssl Python:host"
 PKG_SECTION="lang"
 PKG_SHORTDESC="python: The Python programming language"
 PKG_LONGDESC="Python is an interpreted object-oriented programming language, and is often compared with Tcl, Perl, Java or Scheme."
 
+PKG_INSTALL_VERSION=3.6
+
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="yes"
 
-PY_DISABLED_MODULES="_tkinter nis gdbm bsddb ossaudiodev"
+PKG_CONFIGURE_OPTS_HOST="ac_cv_prog_HAS_HG=/bin/false
+                         ac_cv_prog_SVNVERSION=/bin/false
+                         --disable-pyc-build
+                         --disable-ossaudiodev
+                         --disable-sqlite3
+                         --disable-codecs-cjk
+                         --disable-nis
+                         --enable-unicodedata
+                         --enable-openssl
+                         --disable-readline
+                         --disable-bzip2
+                         --enable-zlib
+                         --disable-xz
+                         --disable-tk
+                         --disable-curses
+                         --disable-pydoc
+                         --disable-test-modules
+                         --enable-lib2to3
+                         --disable-idle3
+                         --without-cxx-main
+                         --with-expat=builtin
+                         --with-libmpdec=none
+                         --with-doc-strings
+                         --without-pymalloc
+                         --without-ensurepi
+"
 
-PKG_CONFIGURE_OPTS_HOST="--cache-file=config.cache \
-                         --without-cxx-main \
-                         --with-threads \
-                         --enable-unicode=ucs4"
+PKG_CONFIGURE_OPTS_TARGET="ac_cv_prog_HAS_HG=/bin/false
+                           ac_cv_prog_SVNVERSION=/bin/false
+                           ac_cv_file__dev_ptmx=no
+                           ac_cv_file__dev_ptc=no
+                           ac_cv_have_long_long_format=yes
+                           ac_cv_working_tzset=yes
+                           ac_cv_func_lchflags_works=no
+                           ac_cv_func_chflags_works=no
+                           ac_cv_func_printf_zd=yes
+                           ac_cv_buggy_getaddrinfo=no
+                           ac_cv_header_bluetooth_bluetooth_h=no
+                           ac_cv_header_bluetooth_h=no
+                           --disable-pyc-build
+                           --disable-ossaudiodev
+                           --enable-sqlite3
+                           --disable-codecs-cjk
+                           --disable-nis
+                           --enable-unicodedata
+                           --enable-openssl
+                           --disable-readline
+                           --enable-bzip2
+                           --enable-zlib
+                           --disable-xz
+                           --disable-tk
+                           --disable-curses
+                           --disable-pydoc
+                           --disable-test-modules
+                           --disable-lib2to3
+                           --disable-idle3
+                           --without-cxx-main
+                           --with-expat=system
+                           --with-libmpdec=none
+                           --with-doc-strings
+                           --without-pymalloc
+                           --without-ensurepip
+                           --with-threads
+                           --enable-ipv6
+"
 
-PKG_CONFIGURE_OPTS_TARGET="ac_cv_file_dev_ptc=no \
-                           ac_cv_file_dev_ptmx=yes \
-                           ac_cv_func_lchflags_works=no \
-                           ac_cv_func_chflags_works=no \
-                           ac_cv_func_printf_zd=yes \
-                           ac_cv_buggy_getaddrinfo=no \
-                           ac_cv_header_bluetooth_bluetooth_h=no \
-                           ac_cv_header_bluetooth_h=no \
-                           ac_cv_file__dev_ptmx=no \
-                           ac_cv_file__dev_ptc=no \
-                           ac_cv_have_long_long_format=yes \
-                           --with-threads \
-                           --enable-unicode=ucs4 \
-                           --enable-ipv6 \
-                           --disable-profiling \
-                           --without-pydebug \
-                           --without-doc-strings \
-                           --without-tsc \
-                           --with-pymalloc \
-                           --without-fpectl \
-                           --with-wctype-functions \
-                           --without-cxx-main \
-                           --with-system-ffi \
-                           --with-system-expat"
-post_patch() {
+post_unpack() {
   # This is needed to make sure the Python build process doesn't try to
   # regenerate those files with the pgen program. Otherwise, it builds
   # pgen for the target, and tries to run it on the host.
@@ -71,51 +108,21 @@ post_patch() {
     touch $PKG_BUILD/Python/graminit.c
 }
 
-make_host() {
-  make PYTHON_MODULES_INCLUDE="$HOST_INCDIR" \
-       PYTHON_MODULES_LIB="$HOST_LIBDIR" \
-       PYTHON_DISABLE_MODULES="readline _curses _curses_panel $PY_DISABLED_MODULES"
+post_makeinstall_host() {
+  rm -f $TOOLCHAIN/bin/python*-config
+  rm -f $TOOLCHAIN/bin/smtpd.py*
+  rm -f $TOOLCHAIN/bin/pyvenv
+  rm -f $TOOLCHAIN/bin/pydoc*
 
-  # python distutils per default adds -L$LIBDIR when linking binary extensions
-    sed -e "s|^ 'LIBDIR':.*| 'LIBDIR': '/usr/lib',|g" -i $(cat pybuilddir.txt)/_sysconfigdata.py
-}
-
-makeinstall_host() {
-  make -j1 PYTHON_MODULES_INCLUDE="$HOST_INCDIR" \
-       PYTHON_MODULES_LIB="$HOST_LIBDIR" \
-       PYTHON_DISABLE_MODULES="readline _curses _curses_panel $PY_DISABLED_MODULES" \
-       install
-}
-
-pre_configure_target() {
-  export PYTHON_FOR_BUILD=$TOOLCHAIN/bin/python
-}
-
-make_target() {
-  make  -j1 CC="$CC" LDFLAGS="$TARGET_LDFLAGS -L." \
-        PYTHON_DISABLE_MODULES="$PY_DISABLED_MODULES" \
-        PYTHON_MODULES_INCLUDE="$TARGET_INCDIR" \
-        PYTHON_MODULES_LIB="$TARGET_LIBDIR"
-}
-
-makeinstall_target() {
-  make  -j1 CC="$CC" DESTDIR=$SYSROOT_PREFIX \
-        PYTHON_DISABLE_MODULES="$PY_DISABLED_MODULES" \
-        PYTHON_MODULES_INCLUDE="$TARGET_INCDIR" \
-        PYTHON_MODULES_LIB="$TARGET_LIBDIR" \
-        install
-
-  make  -j1 CC="$CC" DESTDIR=$INSTALL \
-        PYTHON_DISABLE_MODULES="$PY_DISABLED_MODULES" \
-        PYTHON_MODULES_INCLUDE="$TARGET_INCDIR" \
-        PYTHON_MODULES_LIB="$TARGET_LIBDIR" \
-        install
+  cp $PKG_BUILD/Tools/scripts/reindent.py $TOOLCHAIN/lib/python$PKG_INSTALL_VERSION
 }
 
 post_makeinstall_target() {
-  EXCLUDE_DIRS="bsddb idlelib lib-tk lib2to3 msilib pydoc_data test unittest"
+  PYTHON_DIR=/usr/lib/python$PKG_INSTALL_VERSION
+
+  EXCLUDE_DIRS="ensurepip config compiler distutils sysconfigdata unittest lib2to3 test"
   for dir in $EXCLUDE_DIRS; do
-    rm -rf $INSTALL/usr/lib/python*/$dir
+    rm -rf $INSTALL/$PYTHON_DIR/$dir
   done
 
   rm -rf $INSTALL/usr/lib/python*/config
@@ -124,10 +131,11 @@ post_makeinstall_target() {
   rm -rf $INSTALL/usr/bin/pydoc
   rm -rf $INSTALL/usr/bin/smtpd.py
   rm -rf $INSTALL/usr/bin/python*-config
+  ln -sf /usr/bin/python3 $INSTALL/usr/bin/python
 
-  cd $INSTALL/usr/lib/python2.7
-  python -Wi -t -B $PKG_BUILD/Lib/compileall.py -d /usr/lib/python2.7 -f .
-  find $INSTALL/usr/lib/python2.7 -name "*.py" -exec rm -f {} \; &>/dev/null
+  cd $INSTALL/$PYTHON_DIR
+  python3 -Wi -t -B $PKG_BUILD/Lib/compileall.py -d $PYTHON_DIR -f .
+  find $INSTALL/$PYTHON_DIR -name "*.py" -exec rm -f {} \; &>/dev/null
 
   # strip
   chmod u+w $INSTALL/usr/lib/libpython*.so.*
