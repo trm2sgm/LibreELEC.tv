cbc5365912d61db971cbeda13f3f32db00eb49af
MilhouseVH <milhouseVH.github@nmacleod.com>
2018-02-02 14:41:43 +0000

    RPi: no longer drop rtl8192cu commits

--- tools/RPi/rpi-linux-rebase.sh
+++ tools/RPi/rpi-linux-rebase.sh
@@ -15,13 +15,7 @@ TODO=$1
 # Drop commits not used
 DROP_COMMITS="
 Added Device IDs for August DVB-T 205
-net: Add non-mainline source for rtl8192cu wlan
-net: Fix rtl8192cu build errors on other platforms
-ARM64: Fix build break for RTL8187\/RTL8192CU wifi
 Revert \"softirq: Let ksoftirqd do its job\"
-Revert \"ARM64: Fix build break for RTL8187\/RTL8192CU wifi\"
-Revert \"net: Fix rtl8192cu build errors on other platforms\"
-Revert \"net: Add non-mainline source for rtl8192cu wlan\"
 "
 
 IFS=$'\n'
